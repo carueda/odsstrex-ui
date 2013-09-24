26f0e1d602cb6f98ca0daa7a9f6ac2c3929bce62
Carlos Rueda <carueda@mbari.org>
2013-09-24 11:54:50 -0700

    include readonly configuration property

--- webapp/config.js
+++ webapp/config.js
@@ -1,8 +1,14 @@
 var odsstrexConfig = {
 
-    // odsstres-rest endpoint URL.
-    // Note that odsstrex-rest is CORS-enabled. The default value here simply
-    // assumes that odsstrex-rest runs on the same host:port.
-    rest: "/odsstrex-rest"
+    /*
+     * rest: odsstres-rest endpoint URL.
+     */
+    rest: "/odsstrex-rest",
+
+    /*
+     * readonly: if true, modifications/additions are disabled
+     */
+    readonly: true
+
 };
 
